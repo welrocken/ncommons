a959cc28014777709f777cc06738d618d324ca6f
Musa Ahmetcan Türker <ahmetcantrker@yahoo.com>
2017-12-16 22:08:47 +0300

    s

---
+++ src/validation/Validation/IValidationResult.cs
@@ -0,0 +1,12 @@
+﻿namespace NCommons.Validation
+{
+    public interface IValidationResult<TError>
+    {
+        TError Error { get; }
+        bool Success { get; }
+    }
+
+    public interface IValidationResult : IValidationResult<object>
+    {
+    }
+}
---
+++ src/validation/Validation/IValidator.cs
@@ -0,0 +1,11 @@
+﻿namespace NCommons.Validation
+{
+    public interface IValidator<T, TError>
+    {
+        IValidationResult<TError> Validate(T obj);
+    }
+
+    public interface IValidator<T> : IValidator<T, object>
+    {
+    }
+}
