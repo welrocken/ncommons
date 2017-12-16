565beeca45d14f4ecd4761c231678afd339bfc55
Musa Ahmetcan Türker <ahmetcantrker@yahoo.com>
2017-12-16 21:42:30 +0300

    s

---
+++ src/validation/Validation.FluentValidation/IRuleBuilderExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using FluentValidation;
+
+namespace NCommons.Validation.FluentValidation
+{
+    public static class IRuleBuilderExtensions
+    {
+        public static IRuleBuilder<T, TProperty> WithError<T, TProperty, TError>(this IRuleBuilderOptions<T, TProperty> ruleBuilderOptions, TError error)
+            where TError : struct, IComparable, IFormattable, IConvertible
+        {
+            if (!typeof(TError).IsEnum)
+                throw new ArgumentException("T must be an enumerated type");
+
+            var errorCode = ((int)(object)error).ToString();
+            var errorMessage = error.ToString();
+
+            return ruleBuilderOptions
+                .WithErrorCode(errorCode.ToString())
+                .WithMessage(errorMessage);
+
+        }
+    }
+}
